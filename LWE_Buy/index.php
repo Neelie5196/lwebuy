437095362be0620d2daaa405c9d6e486bd7279c2
Neelie5196 <neelie0501@gmail.com>
2017-11-12 22:09:49 +0800

    insert content into index page

--- LWE_Buy/index.php
+++ LWE_Buy/index.php
@@ -28,33 +28,104 @@
         <div class="row"> 
             <div class="col-xs-12 col-md-12 col-lg-12 parallax">
                 <div class="parallaxcontent">
-                    <p><img src="resources/img/logo.png"/></p>
+                    <p class="center"><img src="resources/img/logo.png"/></p>
                     <h1 class="whiteheader">
                         Logistics Worldwide Express <br/>
                         Buy
                     </h1>
 
+                    <!-- information taken from www.lwe.asia -->
                      <div class="row">
                         <div class="col-xs-12 col-md-12 col-lg-12 homecontent" id="service">
-                            <p>put content here</p>
+                            <h2 class="indextitle">Why Logistics Worldwide Express</h2>
+                            
+                            <div class="row indexrow">
+                                <div class="col-xs-2 col-md-2 col-lg-1 col-lg-push-2">
+                                    <img src="resources/img/why1.png" alt="why1"/>
+                                </div>
+                                
+                                <div class="col-xs-4 col-md-4 col-lg-3 col-lg-push-2">
+                                    <p>We are the one stop solution to multiple shipping service options and destinations</p>
+                                </div>
+                                
+                                <div class="col-xs-2 col-md-2 col-lg-1 col-lg-push-2">
+                                    <img src="resources/img/why2.png" alt="why2"/>
+                                </div>
+                                
+                                <div class="col-xs-4 col-md-4 col-lg-3 col-lg-push-2">
+                                    <p>We have a core team with years of experience in international deliveries solution</p>
+                                </div>
+                            </div>
+                            
+                            <div class="row indexrow">
+                                <div class="col-xs-2 col-md-2 col-lg-1 col-lg-push-2">
+                                    <img src="resources/img/why3.png" alt="why3"/>
+                                </div>
+                                
+                                <div class="col-xs-4 col-md-4 col-lg-3 col-lg-push-2">
+                                    <p>Our services cover more than 200 destinations in the world</p>
+                                </div>
+                                
+                                <div class="col-xs-2 col-md-2 col-lg-1 col-lg-push-2">
+                                    <img src="resources/img/why4.png" alt="why4"/>
+                                </div>
+                                
+                                <div class="col-xs-4 col-md-4 col-lg-3 col-lg-push-2">
+                                    <p>We can customise and tailor our services to suit your needs</p>
+                                </div>
+                            </div>
+                            
+                            <div class="row indexrow">
+                                <div class="col-xs-2 col-md-2 col-lg-1 col-xs-push-3 col-md-push-3 col-lg-push-4">
+                                    <img src="resources/img/why5.png" alt="why5"/>
+                                </div>
+                                
+                                <div class="col-xs-4 col-md-4 col-lg-3 col-xs-push-3 col-md-push-3 col-lg-push-4">
+                                    <p>We have a presence across major cities in the Asia Pacific, with local expertise</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-lg-12 homecontent" id="tracking">
-                            <p>put content here</p>
+                            <h2 class="indextitle">Technology, Ecommerce And LWE</h2>
+                            <div class="row indexrow">
+                                <div class="col-xs-10 col-md-8 col-lg-6 col-md-push-1 col-lg-push-2">
+                                    <p>To fulfill your e-commerce needs, we provide a wide range of solutions to enhance your e-commerce business. If you need help in developing or customizing your e-commerce platform, we have the right expertise to do so.</p>
+
+                                    <p>To make shipping easy, LWE also provides an integrated web shipping solution for you. This solution automatically produces the last mile carrier labels depending on the pre-agree service matrix, weight and destination and service level that you have pre-set. This ensures that the right services labels can be printed with ease.</p>
+
+                                    <p>We also provide ready API’s for you to integrate into your e-commerce platform, where you have the choice to print your labels at the click of a button, check the shipping prices and also track the current status of your packages.</p>
+
+                                    <p>To ensure your customers are pre-notified with the arrival of their parcels, we can provide email and sms notifications, allowing you to fulfil your orders in a more personal and proactive manner.</p>
+                                </div>
+                                
+                                <div class="col-xs-2 col-md-2 col-lg-2 col-md-push-1 col-lg-push-2 right">
+                                    <p><img src="resources/img/home-4.png"/></p>
+                                    <p><img src="resources/img/home-4-2.png"</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-lg-12 homecontent" id="contact">
-                            <p>put content here</p>
+                            <h2 class="indextitle">About Us</h2>
+                            <div class="row indexrow">
+                                <div class="col-xs-12 col-md-12 col-lg-8 col-lg-push-2">
+                                    <p>
+                                        Logistics Worldwide Express (LWE) is a logistics leader in the Asia Pacific market. We specialise in complete cross border logistics and fulfilment. We have established networks in major cities within Asia Pacific. LWE has a wide range of fulfilment solutions that cover a spectrum of service options and destinations. We can customise to suit your business needs
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <p>footer</p>
                 </div>
             </div>
         </div>
+        
+        <div><?php include("footer.php"); ?></div>
     </body>
 </html>
