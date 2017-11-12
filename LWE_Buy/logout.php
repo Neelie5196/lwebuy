1b70666bf86572a24e95a53d8dd01d4b85c38287
clementchuo <clementchuo@gmail.com>
2017-11-12 12:13:26 +0800

    fix logout

--- LWE_Buy/logout.php
+++ LWE_Buy/logout.php
@@ -4,7 +4,7 @@ session_start();
 
 $result = mysqli_query($con, "UPDATE users
 SET login_status = 'Offline'
-WHERE user_id = '$_SESSION[user_id]';");
+WHERE user_id = '$_SESSION[user_id]'");
 session_unset();
 session_destroy();
 header('location: login.php');
